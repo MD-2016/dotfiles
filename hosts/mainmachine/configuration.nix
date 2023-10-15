6af335b30b187c862c45a2c88cf43950d650ada5
MD <25043596+MD-2016@users.noreply.github.com>
2023-10-15 15:41:07 -0400

    Update configuration.nix
    changed gnome to cinnamon

--- hosts/mainmachine/configuration.nix
+++ hosts/mainmachine/configuration.nix
@@ -29,7 +29,8 @@
 
   # Enable the GNOME Desktop Environment.
   services.xserver.displayManager.gdm.enable = true;
-  services.xserver.desktopManager.gnome.enable = true;
+  #services.xserver.desktopManager.gnome.enable = true;
+  services.xserver.desktopManager.cinnamon.enable = true;
 
   # Virtualization stuff
   virtualisation.libvirtd.enable = true;
